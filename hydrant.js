b23769da778d94fa1d41968d9846105216a7851e
Joshua Carroll <JoshuaCarroll@users.noreply.github.com>
2025-03-31 11:24:15 -0500

    Add files via upload

--- hydrant.js
+++ hydrant.js
@@ -35,28 +35,12 @@ function rotateArrow() {
 }
 
 function handleOrientation(event) {
-    let heading = null;
-
-    if (event.absolute || event.webkitCompassHeading) {
-        // iOS
-        heading = event.webkitCompassHeading;
-    } else if (event.alpha !== null) {
-        // Android
-        heading = 360 - event.alpha;
-    }
-
-    if (heading !== null) {
-        currentHeading = heading;
-
-        // Rotate arrow
-        rotateArrow();
-
-        // Rotate compass ring in the opposite direction
-        const compassRing = document.getElementById("compass-ring");
-        if (compassRing) {
-            compassRing.style.transform = `rotate(${-heading}deg)`;
-        }
-    }
+  if (event.absolute || event.webkitCompassHeading) {
+     currentHeading = event.webkitCompassHeading;
+   } else if (event.alpha !== null) {
+     currentHeading = 360 - event.alpha;
+   }
+   rotateArrow();
 }
 
 
